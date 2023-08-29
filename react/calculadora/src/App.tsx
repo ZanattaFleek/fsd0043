3207bb6ddfe2029d0976344de9b8a433ab6c0d02
ZanattaFleek <zanattafleek@gmail.com>
2023-08-29 20:28:36 -0300

    DesenhoCalculadora

--- react/calculadora/src/App.tsx
+++ react/calculadora/src/App.tsx
@@ -1,40 +1,157 @@
-import React from 'react';
-import { Button, IconButton, TextField } from '@mui/material';
+import React, { useState } from "react";
+// import "./App.css";
+// import Botao from './Botao';
+import {
+  Button,
+  Paper,
+  IconButton,
+  Container,
+  TextField,
+  Grid,
+} from "@mui/material";
+import Icon from "@mui/material/Icon";
 
-import Fingerprint from '@mui/icons-material/Fingerprint';
+// import Fingerprint from '@mui/icons-material/Fingerprint';
 
-function App() {
+export default function App() {
+  const [display, setDisplay] = useState<string>("");
+
+  const acrescentarDisplay = (numero: string) => {
+    setDisplay(display.concat(numero));
+  };
+
   return (
-    <div >
+    <>
+      <Container maxWidth="xs">
+        <Paper elevation={3} sx={{ padding: 5, borderRadius: "5%" }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                value={display}
+                id="outlined-basic"
+                variant="outlined"
+                fullWidth
+              />
+            </Grid>
 
-      <TextField id="outlined-basic"  variant="outlined" />
-      <div>
-        <Button size="small" variant="contained">1</Button>
-        <Button size="small" variant="contained">2</Button>
-        <Button size="small" variant="contained">3</Button>
-      </div>
-      <div>
-        <Button size="small" variant="contained">4</Button>
-        <Button size="small" variant="contained">5</Button>
-        <Button size="small" variant="contained">6</Button>
-      </div>
-      <div>
-        <Button size="small" variant="contained">7</Button>
-        <Button size="small" variant="contained">8</Button>
-        <Button size="small" variant="contained">9</Button>
-      </div>
-      <Button size="small" variant="contained">0</Button>
+            <Grid item xs={3}>
+              <Button
+                onClick={() => acrescentarDisplay("1")}
+                fullWidth
+                variant="contained"
+              >
+                1
+              </Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button
+                onClick={() => acrescentarDisplay("2")}
+                fullWidth
+                variant="contained"
+              >
+                2
+              </Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button fullWidth variant="contained">
+                3
+              </Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button fullWidth variant="contained">
+                +
+              </Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button fullWidth variant="contained">
+                4
+              </Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button fullWidth variant="contained">
+                5
+              </Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button fullWidth variant="contained">
+                6
+              </Button>
+            </Grid>
 
-      <IconButton
-        aria-label="fingerprint"
-        color="secondary"
-        sx={{ visibility: { xs: 'hidden', sm: 'visible', md: 'hidden', lg: 'visible' } }}
-      >
-        <Fingerprint />
-      </IconButton>
+            <Grid item xs={3}>
+              <Button fullWidth variant="contained">
+                -
+              </Button>
+            </Grid>
 
-    </div>
+            <Grid item xs={3}>
+              <Button fullWidth variant="contained">
+                7
+              </Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button fullWidth variant="contained">
+                8
+              </Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button fullWidth variant="contained">
+                9
+              </Button>
+            </Grid>
+
+            <Grid item xs={3}>
+              <Button fullWidth variant="contained">
+                *
+              </Button>
+            </Grid>
+
+            <Grid item xs={3}>
+              <Button fullWidth variant="contained">
+                CE
+              </Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button fullWidth variant="contained">
+                0
+              </Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button fullWidth variant="contained">
+                =
+              </Button>
+            </Grid>
+
+            <Grid item xs={3}>
+              <Button fullWidth variant="contained">
+                /
+              </Button>
+            </Grid>
+
+            <Grid item xs={3}>
+              <Button fullWidth variant="contained">
+                x^2
+              </Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button fullWidth variant="contained">
+                RAIZ
+              </Button>
+            </Grid>
+            <Grid item xs={3}>
+              <Button fullWidth variant="contained">
+                .
+              </Button>
+            </Grid>
+
+            <Grid item xs={3}>
+              <Button fullWidth variant="contained">
+                BACK
+              </Button>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Container>
+    </>
   );
 }
-
-export default App;
